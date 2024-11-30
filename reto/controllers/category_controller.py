1c6a960ac97faa7accf97cf1577af4c00fbdf823
unknown <alejoszrz@gmail.com>
2024-11-30 14:16:39 -0500

    Avance servicio y controller category

--- reto/controllers/category_controller.py
+++ reto/controllers/category_controller.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter
 
 from reto.models.category_schema import CategoryRead
+from services.category_service import CategoryService
+
 
 route = APIRouter()
 
 @route.get("/categories", response_model=CategoryRead)
-async def get_category():
-    pass
+async def get_categories():
+    return CategoryService.get_all_categories()
 
 @route.post("/")
 def create():
--- reto/services/category_service.py
+++ reto/services/category_service.py
@@ -1,7 +1,12 @@
+from sqlmodel import select
+from database.db import DBSession
+from reto.models.category_model import Category
 
 
 class CategoryService():
-    def __init__(self, DBDep):
-        self.DBDep = DBDep
-    
-    
+    def __init__(self, db: DBSession):
+        self.db = db
+
+    def get_all_categories(self):
+        categories = self.db.exec(select(Category).all())
+        return categories
